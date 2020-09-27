47d7f0483cdd06b55c9ef42b5b93f042b2cfad82
Gatsjy <gkswndks123@naver.com>
2020-09-27 20:30:18 +0900

    2020-09-27, 한주안, 다운 캐스팅과 instanceof

--- Chapter7/src/polymorphism/animalTest.java
+++ Chapter7/src/polymorphism/animalTest.java
@@ -12,17 +12,29 @@ class Human extends Animal{
     public void move(){
         System.out.println("사람이 움직입니다.");
     }
+
+    public void readBooks(){
+        System.out.println("사람이 책을 읽습니다.");
+    }
 }
 
 class Tiger extends Animal{
     public void move(){
         System.out.println("호랑이가 네 발로 뜁니다.");
     }
+
+    public void hunting(){
+        System.out.println("호랑이가 사냥을 합니다.");
+    }
 }
 
 class Eagle extends Animal{
     public void move(){
         System.out.println("독수리가 하늘을 날아갑니다.");
+    }
+
+    public void flying(){
+        System.out.println("독수리가 날개를 쭉 펴고 멀리 날아갑니다.");
     }
 }
 
@@ -46,10 +58,18 @@ public class animalTest {
         animalList.add(tAnimal);
         animalList.add(eAnimal);
 
-        // 가상 메서드에 매칭되는 함수가 출력이 됩니다.1
-        for(Animal animal : animalList){
-            animal.move();
+        if ( hAnimal instanceof Human){
+            Human human = (Human)hAnimal;
+            human.readBooks();
         }
+
+        animalTest animalTest = new animalTest();
+        animalTest.testDownCasting(animalList);
+
+        // 가상 메서드에 매칭되는 함수가 출력이 됩니다.1
+        //for(Animal animal : animalList){
+        //    animal.move();
+        //}
     }
 
     // 들어오는 것은 animal인데?? 각각 다른 메서드가 실행됩니다.
@@ -57,5 +77,22 @@ public class animalTest {
     // 하나의 코드가 다양한 구현을 할 수 있음 -> 다형성
     public void moveAnimal(Animal animal){
         animal.move();
+    }
+
+    public void testDownCasting(ArrayList<Animal> list){
+
+        for(int i = 0 ; i < list.size(); i++){
+            Animal animal = list.get(i);
+            if(animal instanceof Human){
+                Human human = (Human)animal;
+                human.readBooks();
+            }else if(animal instanceof Tiger){
+                Tiger tiger = (Tiger)animal;
+                tiger.hunting();
+            }else {
+                Eagle eagle = (Eagle)animal;
+                eagle.flying();
+            }
+        }
     }
 }
