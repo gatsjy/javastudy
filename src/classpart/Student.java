a1d4ba396d808edfcfcda375358fe5e1912ed33c
Gatsjy <gkswndks123@naver.com>
2020-09-27 10:03:25 +0900

    2020-09-27, 한주안, 생성자, 생성자 오버로딩

--- src/classpart/Student.java
+++ src/classpart/Student.java
@@ -6,7 +6,19 @@ public class Student {
     public String studentName;
     public String address;
 
-    public void showStudentInfo(){
+    // 개발자가 기본 생성자를 구현 하고 안하고를 결정할 수 있다.
+    public Student(){};
+
+    public Student(String name){
+        studentName = name;
+    }
+
+    public Student(int id, String name){
+        studentId = id;
+        studentName = name;
+        address ="주소 없음";
+    }
+    public void showStudentInfo() {
         System.out.println("studentId = " + studentId);
         System.out.println("studentName = " + studentName);
         System.out.println("address = " + address);
--- src/classpart/StudentTest.java
+++ src/classpart/StudentTest.java
@@ -2,10 +2,21 @@ package classpart;
 
 public class StudentTest {
 
+    // 힙에 올려진 메모리를 인스터스라고 명명한다.
     public static void main(String[] args) {
         Student studentLee = new Student();
         studentLee.studentName = "한주안";
         studentLee.address = "서울";
+
         studentLee.showStudentInfo ();
+
+        Student studentKim = new Student("김유신");
+        studentKim.address ="경주";
+
+        studentKim.showStudentInfo();
+
+        // 참조 변수 값 출력
+        System.out.println(studentKim);
+        System.out.println(studentLee);
     }
 }
