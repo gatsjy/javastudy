6b9180bb7940fa894a7a2045a7179ae9a1e5242e
Gatsjy <gkswndks123@naver.com>
2020-09-27 09:29:30 +0900

    2020-09-27, 자바 기본 study 시작

---
+++ src/classpart/Student.java
@@ -0,0 +1,15 @@
+package classpart;
+
+public class Student {
+
+    public int studentId;
+    public String studentName;
+    public String address;
+
+    public void showStudentInfo(){
+        System.out.println("studentId = " + studentId);
+        System.out.println("studentName = " + studentName);
+        System.out.println("address = " + address);
+    }
+
+}
