1feea82fa45756a1e5fea11b499533a3704531f3
Gatsjy <gkswndks123@naver.com>
2020-09-29 20:38:15 +0900

    2020-09-29, 한주안, List 인터페이스

---
+++ Chapter11/src/collection/LinkedListTest.java
@@ -0,0 +1,25 @@
+package collection;
+
+import java.util.LinkedList;
+
+public class LinkedListTest {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> myList = new LinkedList<String>();
+
+        myList.add("A");
+        myList.add("B");
+        myList.add("C");
+
+        System.out.println(myList.toString());
+        myList.add(1,"D");
+        System.out.println(myList);
+        myList.removeLast();
+        System.out.println(myList);
+
+        for(int i = 0 ; i < myList.size(); i++){
+            System.out.println(myList.get(i).toString());
+        }
+    }
+}
