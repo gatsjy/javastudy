47bd23a3a8c234c8a6ee03f3a81bc1f0914142e6
Gatsjy <gkswndks123@naver.com>
2020-10-02 20:09:53 +0900

    2020-09-30, 한주안, 직렬화

---
+++ Chapter14/src/stream/serialization/Person.java
@@ -0,0 +1,31 @@
+package stream.serialization;
+
+import java.io.*;
+
+public class Person implements Externalizable {
+    String name;
+    String job;
+
+    public Person(String name, String job) {
+        this.name = name;
+        this.job = job;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", job='" + job + '\'' +
+                '}';
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+
+    }
+}
---
+++ Chapter14/src/stream/serialization/serializationTest.java
@@ -0,0 +1,34 @@
+package stream.serialization;
+
+import java.io.*;
+
+public class serializationTest{
+
+    public static void main(String[] args) {
+
+        Person personLee = new Person("이순신", "엔지니어");
+        Person personKim = new Person("김유신", "선생님");
+
+        try(FileOutputStream fos = new FileOutputStream("serial.dat");
+            ObjectOutputStream oos = new ObjectOutputStream(fos)){
+            oos.writeObject(personLee);
+            oos.writeObject(personKim);
+        }catch (IOException e){
+            System.out.println(e);
+        }
+
+        try(FileInputStream fis = new FileInputStream("serial.dat");
+            ObjectInputStream ois = new ObjectInputStream(fis)){
+
+            Person p1 = (Person)ois.readObject();
+            Person p2 = (Person)ois.readObject();
+
+            System.out.println(p1);
+            System.out.println(p2);
+        }catch (IOException e){
+            System.out.println(e);
+        }catch (ClassNotFoundException e){
+            System.out.println(e);
+        }
+    }
+}
