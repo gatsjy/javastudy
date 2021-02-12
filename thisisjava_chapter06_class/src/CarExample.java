b9af4ce40c14b67e2fef834edaf830fee3259912
Gatsjy <Gatsjy@DESKTOP-4TT9522>
2021-02-12 09:02:24 +0900

    2021-02-12, 한주안, 이것이 자바다2

---
+++ thisisjava_chapter06_class/src/CarExample.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Gatsjy on 2021-02-12
+ * Blog : https://blog.naver.com/gkswndks123
+ * Github : https://github.com/gatsjy
+ */
+public class CarExample {
+    public static void main(String[] args) {
+        Car myCar = new Car("검정", 3000);
+        // Car myCar = new Car(); // 기본 생성자를 만들어주지 않았기 때문에 생성할 수 없다.
+
+    }
+}
---
+++ thisisjava_chapter06_class/src/Korean.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Gatsjy on 2021-02-12
+ * Blog : https://blog.naver.com/gkswndks123
+ * Github : https://github.com/gatsjy
+ */
+public class Korean {
+    String nation = "대한민국";
+    String name;
+    String ssn;
+
+    // 생성자
+    public Korean(String n, String s){
+        name = n;
+        ssn = s;
+    }
+}
+
